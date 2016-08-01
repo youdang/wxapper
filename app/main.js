ecf68d636d9a2e2f2b4f584f6192a29586a91f42
youdang <youdang0809@gmail.com>
2016-08-01 11:16:21 +0800

    [BUG] fix bugs

--- app/main.js
+++ app/main.js
@@ -3,40 +3,34 @@ const {app, BrowserWindow} = require('electron')
 let mainWindow
 
 function createWindow() {
-    if (mainWindow != null) {
-        return
-    }
-
     mainWindow = new BrowserWindow({
         width: 960,
         height: 728,
+        title: 'Wxapper',
         webPreferences: {
             allowDisplayingInsecureContent: true,
             allowRunningInsecureContent: true,
-            nodeIntegration: false,
-            plugins: true
+            nodeIntegration: false
         }
     })
 
     mainWindow.loadURL('http://wx.qq.com')
 
-    mainWindow.on('close', () => {
+    mainWindow.on('closed', () => {
         mainWindow = null
     })
 }
 
-app.on('ready', () => {
-    createWindow()
-})
+app.on('ready', createWindow)
 
 app.on('window-all-closed', () => {
-    if (process.platform != 'darwin') {
+    if (process.platform !== 'darwin') {
         app.quit()
     }
 })
 
-app.on('activated', () => {
-    if (mainWindow == null) {
+app.on('activate', () => {
+    if (mainWindow === null) {
         createWindow()
     }
 })
